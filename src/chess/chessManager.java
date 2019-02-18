df295f451d2df69fc3bd44260508df4b5a577f2a
stefan01 <stefan01@stefan.lan>
2019-02-18 22:39:07 +0100

    s

--- src/chess/chessManager.java
+++ src/chess/chessManager.java
@@ -7,10 +7,20 @@ public class chessManager {
 	private BoardManager bord;
 	private Piece[] white;
 	private Piece[] black;
-	private int pointB;
-	private int pointW;
+	private int pointB = 0;
+	private int pointW = 0;
 	
 	public chessManager(){
+		char[][] tmp = {{'T','C','F','D','R','F','C','T'},
+						{'P','P','P','P','P','P','P','P'},
+						{' ',' ',' ',' ',' ',' ',' ',' '},
+						{' ',' ',' ',' ',' ',' ',' ',' '},
+						{' ',' ',' ',' ',' ',' ',' ',' '},
+						{' ',' ',' ',' ',' ',' ',' ',' '},
+						{'P','P','P','P','P','P','P','P'},
+						{'T','C','F','D','R','F','C','T'}};
+		
+		this.bord = new BoardManager(tmp);
 		
 	}
 	
