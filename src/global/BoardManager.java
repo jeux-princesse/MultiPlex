791bd3440a7f5ec58a7378cab8414b5c7cd215c9
Goldocelot <nico.19.9.2000@hotmail.com>
2019-02-18 20:50:13 +0100

    V0.1 BoardManager 18/02/2019

---
+++ src/global/BoardManager.java
@@ -0,0 +1,31 @@
+package global;
+
+import java.util.Arrays;
+
+public class BoardManager <T extends Object>{
+	private T[][] board;
+	
+	@SuppressWarnings("unchecked")
+	public BoardManager(int height, int widht) {
+		
+		this.board = (T[][])new Object[height][];
+		for(int i=0;i<height;i++) {
+			this.board[i] = (T[])new Object[widht];
+		}
+		
+	}
+	
+	public void show() {
+		for(int i = 0 ; i<board.length; i++) {
+			System.out.println(Arrays.toString(board[i]));
+		}
+	}
+	
+	public T[][] getBoard(){
+		return board;
+	}
+	
+	public void insert(int line, int column, T things) {
+		this.board[line][column] = things;
+	}
+}
--- src/global/Main.java
+++ src/global/Main.java
@@ -3,7 +3,13 @@ package global;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BoardManager<Character> bm = new BoardManager<>(5, 5);
+		bm.insert(2, 3, 'a');
+		bm.show();
+		System.out.println();
+		BoardManager<String> bm2 = new BoardManager<>(5, 5);
+		bm2.insert(2, 3, "AA");
+		bm2.show();
 
 	}
 
