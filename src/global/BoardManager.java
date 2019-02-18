b052369052d086f3927233e07707260b8be3ca73
Goldocelot <nico.19.9.2000@hotmail.com>
2019-02-18 22:09:16 +0100

    BoardManager V0.1 18/02/2019

--- src/global/BoardManager.java
+++ src/global/BoardManager.java
@@ -1,31 +1,67 @@
 package global;
 
-import java.util.Arrays;
-
-public class BoardManager <T>{
-	private T[][] board;
+public class BoardManager {
+	private char[][] board;
+	private final char[] letters= {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 	
-	@SuppressWarnings("unchecked")
 	public BoardManager(int height, int widht) {
-		
-		this.board = (T[][])new Object[height][];
-		for(int i=0;i<height;i++) {
-			this.board[i] = (T[])new Object[widht];
+		if(widht>26) {
+			widht = 26;
 		}
+		this.setBoard(height, widht);
+		this.reset();
 		
 	}
 	
 	public void show() {
-		for(int i = 0 ; i<board.length; i++) {
-			System.out.println(Arrays.toString(board[i]));
+		String firstLine ="-";
+		String lastLine ="-";
+		int height = board.length;
+		int widht = board[0].length;
+		int numberMaxWidht = String.valueOf(height).length();
+		
+		for(int i = 0 ; i<numberMaxWidht ; i++) {
+			firstLine+="-";
+			lastLine+="-";
+		}		
+		for(int i = 0 ; i<widht ; i++) {
+			firstLine+=this.letters[i]+"-";
+			lastLine+="--";
+		}
+		System.out.println(firstLine);
+		for(int i = 0 ; i<height ; i++) {
+			String line = String.valueOf(i+1);
+			int numberWidht = line.length();
+			for(int j = 0 ; j<numberMaxWidht-numberWidht; j++) {
+				line+=" ";
+			}
+			line+="|";
+			for(int j = 0; j<widht ; j++) {
+				line+=" |";
+			}
+			System.out.println(line);
+			System.out.println(lastLine);
 		}
 	}
 	
-	public T[][] getBoard(){
-		return board;
+	public void reset() {
+		for(int i = 0 ; i<board.length; i++) {
+			for(int j = 0; j<board[i].length; j++) {
+				this.board[i][j] = ' ';
+			}
+		}
 	}
 	
-	public void insert(int line, int column, T things) {
-		this.board[line][column] = things;
+	public void insert(Piece piece) {
+		this.board[piece.getCoord()[0]][piece.getCoord()[1]] = piece.getSkin();
 	}
+	
+	public void setBoard(int height, int widht) {
+		this.board = new char[height][];
+		for(int i=0;i<height;i++) {
+			this.board[i] = new char[widht];
+		}
+	}
+	
+	
 }
--- src/global/Main.java
+++ src/global/Main.java
@@ -3,13 +3,8 @@ package global;
 public class Main {
 
 	public static void main(String[] args) {
-		BoardManager<Character> bm = new BoardManager<>(5, 5);
-		bm.insert(2, 3, 'a');
+		BoardManager bm = new BoardManager(100, 100);
 		bm.show();
-		System.out.println();
-		BoardManager<String> bm2 = new BoardManager<>(5, 5);
-		bm2.insert(2, 3, "AA");
-		bm2.show();
 
 	}
 
